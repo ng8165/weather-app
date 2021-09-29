9909ae6608c1974bcadea7f8dccff4bf608ca395
Nelson Gou <nel4gou@gmail.com>
2021-09-29 15:11:54 -0700

    Use weather.gov API to extract weather data

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import requests
 
-# print("Hello World")
+loc = requests.get("https://api.weather.gov/points/37.2980448,-122.0078557")
+weather = requests.get(loc.json()["properties"]["forecast"])
 
-result = requests.get("https://google.com")
-# print(result)
-print(result.text)
+print("Weather for " + loc.json()["properties"]["relativeLocation"]["properties"]["city"] + ", ", end="")
+print(loc.json()["properties"]["relativeLocation"]["properties"]["state"] + ":\n")
+for i in range(14):
+    day = weather.json()["properties"]["periods"][i]
+    print(day["name"] + ": ", end="")
+    print(str(day["temperature"]) + "°" + day["temperatureUnit"] + ", ", end="")
+    print(day["shortForecast"])
